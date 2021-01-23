0d4f42111fae6971f87c122fc7906c87a87c11cc
Matheus Novaes <matheuspnovaes@gmail.com>
2021-01-23 16:11:07 -0300

    feat: processar os reviews

--- utils.py
+++ utils.py
@@ -1,4 +1,24 @@
 
+import sys, getopt
+import os
+import re
+import joblib
+import pickle
+
+import numpy as np
+import pandas as pd
+
+from os import listdir
+
+from sklearn.metrics import accuracy_score
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.svm import LinearSVC
+
+
+
+
+
 # leitura dos reviews
 def open_reviews(directory):
     reviews_list = []
@@ -12,3 +32,15 @@ def open_reviews(directory):
             for line in file:
                 reviews_list.append(textProcess(line.strip()))              
     return reviews_list
+
+# remover pontuações e tags html do review
+def textProcess(review):
+    #expressão regular para remover pontuções
+    exp_pontuacao = re.compile("[.;:!\'?,\"()\[\]]")
+	#expressão regular para remover tags html 
+    exp_htmlTag= re.compile("(<br\s*/><br\s*/>)|(\-)|(\/)")
+	
+    review = exp_pontuacao.sub("", review.lower())
+    review = exp_htmlTag.sub(" ",review)
+	
+    return review
