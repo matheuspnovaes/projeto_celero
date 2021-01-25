2c1b47f8ef7d116c813d540cf0e2db7aaa9abb20
Matheus Novaes <matheuspnovaes@gmail.com>
2021-01-25 11:22:53 -0300

    fix: abertura arquivo rotulos #2

--- model.py
+++ model.py
@@ -1,3 +1,20 @@
+import sys, getopt
+import os
+import re
+import numpy as np
+
+import joblib
+import pickle
+#import pandas as pd
+
+from sklearn.metrics import accuracy_score
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.svm import LinearSVC
+
+
+from utils import open_reviews, textProcess, vectorizer_data
+
 # opcao do modelo em modo de treinamento
 def modo_treinamento(argv):
     directory = ''
@@ -6,14 +23,14 @@ def modo_treinamento(argv):
     if (len(argv)==4):
         directory = argv[2]
         file_labels = argv[3]
+        print(file_labels)
         path_labels = os.path.join(directory, file_labels)
-        if not os.path.exists(file_labels):
-            print("O arquivo não existe ou não está no diretorio %s" % directory)
-        else:
-            try:
-                label = np.genfromtxt(file_labels,  delimiter=',')
-            except:
-                print("Falha ao abrir o arquivo")
+        
+        try:
+            label = np.genfromtxt(file_labels,  delimiter=',')
+        except:
+            print("Falha ao abrir o arquivo de rotulos")
+            sys.exit(2)
         if not os.path.exists(directory):
             print("O diretório não existe")
             sys.exit(2)
@@ -39,6 +56,30 @@ def modo_treinamento(argv):
         print("Erro: O camando de entrada deve ser da seguinte forma:")
         print("python model.py <opção> <\"diretorio\"> <rotulos>")
         sys.exit(2)  
+        
+def modo_execucao(argv):
+    reviews_list = []
+    file_teste = ''
+    review_vectorizer = joblib.load("review_vectorizer.pkl")
+    print(len(argv))
+    if (len(argv)==3):
+        file_teste = argv[2]
+        if not os.path.exists(file_teste):
+            print("O arquivo não existe ou não está no diretorio")
+        else:
+            with open(file_teste,'r',encoding="utf8") as file:
+                for line in file:
+                    reviews_list.append(textProcess(line.strip()))    
+            print("Vetorizando os dados...")
+            X_data = review_vectorizer.transform(reviews_list)
+            svm_model = pickle.load(open('svm_model_trained.sav', 'rb'))
+            result = svm_model.predict(X_data)
+            print("Classificação do review: %s"% result[0])
+    else:
+        print("Erro: O camando de entrada deve ser da seguinte forma:")
+        print("python model.py <opção> <arquivo_teste>")
+        sys.exit(2)  
+  
 
 
 def model_training(X_train, y_train):       
@@ -51,7 +92,10 @@ def model_training(X_train, y_train):
 optionDic={
     '1': modo_treinamento,
     '2': modo_execucao
-}    
+}
+
+
+        
 def main(argv):
     optionDic[argv[1]](argv)        
 if __name__ == "__main__":
